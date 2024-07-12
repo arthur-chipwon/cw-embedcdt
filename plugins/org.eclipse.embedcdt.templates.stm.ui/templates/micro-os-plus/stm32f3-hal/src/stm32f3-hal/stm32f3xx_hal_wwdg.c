08fe4a3cd31e45554e0775139817bdd81e2d1428
Arthur Hsiao <givenchy5168@gmail.com>
2024-07-12 17:44:39 +0800

    style: format style
    As subject.

--- plugins/org.eclipse.embedcdt.templates.stm.ui/templates/micro-os-plus/stm32f3-hal/src/stm32f3-hal/stm32f3xx_hal_wwdg.c
+++ plugins/org.eclipse.embedcdt.templates.stm.ui/templates/micro-os-plus/stm32f3-hal/src/stm32f3-hal/stm32f3xx_hal_wwdg.c
@@ -33,7 +33,7 @@
         (++) max time (mS) = 1000 * (Counter - 0x40) / WWDG clock
     (+) Typical values:
         (++) Counter min (T[5;0] = 0x00) @36MHz (PCLK1) with zero prescaler:
-             max timeout before reset: approximately 113.78ï¿½s
+             max timeout before reset: approximately 113.78µs
         (++) Counter max (T[5;0] = 0x3F) @36MHz (PCLK1) with prescaler dividing by 8:
              max timeout before reset: approximately 58.25ms
 
